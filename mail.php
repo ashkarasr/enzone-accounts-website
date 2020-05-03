80fc99ef44d7b708f08be383b95820c7a836804d
ashkarasr <ashkarasr@gmail.com>
2020-05-03 23:58:18 +0530

    final

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+$to = "nisamudheenn@hotmail.com"; 
+$from = $_POST['email'];
+$full_name = $_POST['name'];         
+$subject = "Form submission";     
+$subject2 = "Copy of your form submission";     
+$message = "Name : ". $full_name . "\n\nMessage:" . $_POST['cmt'];  
+$headers = "From:" . $from;      
+$mail = mail($to,$subject,$message,$headers);     
+if (isset($mail)) { 
+echo "Form Submitted Succesfully";
+} else { 
+echo "Mail Not Send";
+}?>
